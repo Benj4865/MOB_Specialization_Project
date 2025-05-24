96d92b60dc6e0a89760f813a607ab4e9e10ffedf
Benj4865 <bena380dk@gmail.com>
2025-05-24 15:21:45 +0200

    Added to compass code

--- Code/Arduino/GPS_Compass_Reader/GPS_Compass_Reader.ino
+++ Code/Arduino/GPS_Compass_Reader/GPS_Compass_Reader.ino
@@ -12,24 +12,52 @@
 #include "Wire.h"
 
 // Standard address for the compass is 0x0c so we will define it here.
-AK8975 mag(0x0c);
+AK8975 comp(0x0c);
 
 // Address for the MPU is 0x68
 MPU6050 mpu;
 
-// Setting up the variable to hold the compass heading in 3 axis. 
-int16_t mx, my, mz;
+// Setting up the 16-bit integers to hold the compass heading in 3 axis. 
+int16_t vx, vy, vz;
 float c_heading;
 
-void setup() {
+void setup()
+{
 
-  
+  // Opening I2C connection 
   Wire.begin();
   
+  // Starting serial communication with computer
+  Serial.begin(9600);
 
+  
+  Serial.println("Wire connection and Serial connection started....");
+
+  mpu.initialize();
+  mpu.setI2CBypassEnabled(true);
+  comp.initialize();
+
+  Serial.println(comp.testConnection());
+  
 }
 
-void loop() {
-  // put your main code here, to run repeatedly:
+void loop()
+{
 
+  //Defining the memory address of the variables
+  //So that they can be changed at once. Instead of running the 
+  //getHeading 3 times.
+  comp.getHeading(&vx, &vy, &vz);
+
+  // Calculation to go from vectors in getHeading to degrees 
+  c_heading = atan2((double)vy, (double)vx) * 180.0/3.14159265 + 180;
+  while (c_heading < 0) c_heading += 360;
+  while (c_heading > 360) c_heading -= 360;
+
+  Serial.print(c_heading);
+  Serial.println("°");
+
+  // Works best if running about 10 times pr second.
+  delay(100);
+  
 }
